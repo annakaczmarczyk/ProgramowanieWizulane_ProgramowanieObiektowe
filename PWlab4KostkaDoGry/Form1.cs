91ff2cdaf0b50ab7b993763ef35e335339273132
AniaKacz <62969412+AniaKacz@users.noreply.github.com>
2020-04-01 02:01:32 +0200

    pwLAB4
    KOSTKA

---
+++ PWlab4KostkaDoGry/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KostkaDoGry
+{
+    public partial class Form1 : Form
+    {
+        private Random _rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'r')
+            {
+                label1.Text = RzucKostka().ToString();
+                label1.ForeColor = Color.FromArgb(WylosujBajt(), WylosujBajt(), WylosujBajt());
+            }
+        }
+
+        private int RzucKostka()
+        {
+            return _rnd.Next(1, 7);
+        }
+
+        private int WylosujBajt()
+        {
+            return _rnd.Next(0, 256);
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+            Clipboard.SetText(label1.Text);
+
+            MessageBox.Show(this, "Skopiowano " + label1.Text + "!");
+        }
+    }
+}
