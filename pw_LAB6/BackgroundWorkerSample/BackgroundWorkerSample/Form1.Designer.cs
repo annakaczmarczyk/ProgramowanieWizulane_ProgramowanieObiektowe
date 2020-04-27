29444bb7f72073d18ac774638db1c10687c67ae4
AniaKacz <62969412+AniaKacz@users.noreply.github.com>
2020-04-27 16:29:38 +0200

    pw_LAB6
    pw_LAB6

---
+++ pw_LAB6/BackgroundWorkerSample/BackgroundWorkerSample/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BackgroundWorkerSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartAsyncOperation = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartAsyncOperation
+            // 
+            this.btnStartAsyncOperation.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnStartAsyncOperation.FlatAppearance.BorderColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnStartAsyncOperation.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnStartAsyncOperation.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnStartAsyncOperation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartAsyncOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartAsyncOperation.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnStartAsyncOperation.Location = new System.Drawing.Point(39, 155);
+            this.btnStartAsyncOperation.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStartAsyncOperation.Name = "btnStartAsyncOperation";
+            this.btnStartAsyncOperation.Size = new System.Drawing.Size(324, 64);
+            this.btnStartAsyncOperation.TabIndex = 0;
+            this.btnStartAsyncOperation.Text = "Start ";
+            this.btnStartAsyncOperation.UseVisualStyleBackColor = false;
+            this.btnStartAsyncOperation.Click += new System.EventHandler(this.btnStartAsyncOperation_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.progressBar1.Location = new System.Drawing.Point(13, 82);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(817, 38);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCancel.Enabled = false;
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(447, 155);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(336, 64);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Stop";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblStatus.Location = new System.Drawing.Point(357, 24);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(85, 29);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Start....";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 239);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnStartAsyncOperation);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "BackgroundWorker Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartAsyncOperation;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ pw_LAB6/BackgroundWorkerSample/BackgroundWorkerSample/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace BackgroundWorkerSample
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// The backgroundworker object on which the time consuming operation shall be executed
+        /// </summary>
+        BackgroundWorker m_oWorker;
+        ArrayList tablP = new ArrayList();
+        ArrayList tablRP = new ArrayList();
+        Random rng = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Prime Numbers Generator Progress Bar";
+            m_oWorker = new BackgroundWorker();
+            m_oWorker.DoWork += new DoWorkEventHandler(m_oWorker_DoWork);
+            m_oWorker.ProgressChanged += new ProgressChangedEventHandler(m_oWorker_ProgressChanged);
+            m_oWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(m_oWorker_RunWorkerCompleted);
+            m_oWorker.WorkerReportsProgress = true;
+            m_oWorker.WorkerSupportsCancellation = true;
+        }
+
+        /// <summary>
+        /// On completed do the appropriate task
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //If it was cancelled midway
+            if (e.Cancelled)
+            {
+                lblStatus.Text = "Task Cancelled.";
+            }
+            else if (e.Error != null)
+            {
+                lblStatus.Text = "Error while performing background operation.";
+            }
+            else
+            {
+                lblStatus.Text = "Task Completed...";
+            }
+            btnStartAsyncOperation.Enabled = true;
+            btnCancel.Enabled = false;
+        }
+
+        /// <summary>
+        /// Notification is performed here to the progress bar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            //Here you play with the main UI thread
+            progressBar1.Value = e.ProgressPercentage;
+            lblStatus.Text = "Processing......" + progressBar1.Value.ToString() + "%";
+        }
+
+        /// <summary>
+        /// Time consuming operations go here </br>
+        /// i.e. Database operations,Reporting
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void m_oWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            bool t;
+            int low = 1;
+            int high = 1000;
+            
+            while(low < high)
+            {
+                t = true;
+                for(int i=2;i< low; i++)
+                {
+                    if(low % i==0)
+                    {
+                        t = false;
+                        break;
+                    }
+                }
+                if(t)
+                {
+                    tablP.Add(low);
+                }
+                low++;
+                Thread.Sleep(1);
+                m_oWorker.ReportProgress(low/100);
+                if (m_oWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    m_oWorker.ReportProgress(0);
+                    return;
+                }
+            }
+            m_oWorker.ReportProgress(100);
+            MessageBox.Show("Prime numbers have been generated!!");
+            m_oWorker.ReportProgress(0);
+            for(int i=0;i<100;i++)
+            {
+                Thread.Sleep(i);
+                tablRP.Add(tablP[rng.Next(0, tablP.Count - 1)]);
+                m_oWorker.ReportProgress(i);
+
+                if (m_oWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    m_oWorker.ReportProgress(0);
+                    return;
+                }
+            }
+            m_oWorker.ReportProgress(100);
+        }
+
+        private void btnStartAsyncOperation_Click(object sender, EventArgs e)
+        {
+            btnStartAsyncOperation.Enabled  = false;
+            btnCancel.Enabled               = true;
+            m_oWorker.RunWorkerAsync();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (m_oWorker.IsBusy)
+            {
+                m_oWorker.CancelAsync();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
