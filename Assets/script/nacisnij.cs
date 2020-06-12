f51ba4e3ac628d085fe08f5c43ca20d3c2c459cd
annakaczmarczyk <56322001+annakaczmarczyk@users.noreply.github.com>
2020-06-12 17:06:13 +0200

    lab10_pw
    lab10_pw

---
+++ Assets/script/nacisnij.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class nacisnij : MonoBehaviour
+{
+    public GameObject prefab;
+    public int limitX;
+    public float speedMoveCube;
+    public Material[] colors;
+
+    private void Start()
+    {
+        GameObject obj = Instantiate(prefab, transform.position, Quaternion.identity);
+        obj.GetComponent<Renderer>().material = colors[RandomColor()];
+        obj.transform.Translate(new Vector3(RandomReturn(), 0, 0));
+        obj.GetComponent<ruch>().speed = speedMoveCube;
+    }
+
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Mouse0))
+       {
+            GameObject obj = Instantiate(prefab,transform.position,Quaternion.identity);
+            obj.GetComponent<Renderer>().material = colors[RandomColor()];
+            obj.transform.Translate(new Vector3(RandomReturn(),0,0));
+            obj.GetComponent<ruch>().speed = speedMoveCube;
+
+       }
+    }
+
+    int RandomColor()
+    {
+        return Random.Range(0, colors.Length);
+    }
+    int RandomReturn()
+    {
+        return Random.Range(-limitX,limitX);
+    }
+}
---
+++ Assets/script/ruch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ruch : MonoBehaviour
+{
+    public float speed;
+    float x, y;
+    void FixedUpdate()
+    {
+     
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Translate(x * speed * Time.fixedDeltaTime, y * speed * Time.deltaTime, 0);
+        
+    }
+}
